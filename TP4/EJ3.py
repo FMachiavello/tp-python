dbcce280e9125dd54766c0fa5b85e3cbf727d220
Val <valiktroi16@gmail.com>
2019-05-09 18:24:22 -0300

    PARA DIEGO

--- TP4/EJ3.py
+++ TP4/EJ3.py
@@ -1,9 +1,13 @@
-def numTriangular(suma):
+def numTriangular(n):
         """Calcula los numeros triangulares ingresados en la variable suma y
         los muestra"""
-        while suma.isnumeric() is False:  # Detecta si el dato es numerico
-                suma = input("Error, ingrese un numero: ")
-        for i in range(1, int(suma) + 1):  # Recorre de 1 hasta el dato
+        if n.isnumeric() is False:  # Detecta si el dato es numerico
+                return None
+        for i in range(1, int(n) + 1):  # Recorre de 1 hasta el dato
                 cont = i
-                suma = int(suma) + cont
+                suma = int(cont) * (int(cont) + 1) / 2
                 print(cont, " - ", suma)  # Muestra los numeros triangulares
+                return(int(n) * (int(n) + 1) / 2)
+
+if __name__ == '__main__':
+        numTriangular(4)
