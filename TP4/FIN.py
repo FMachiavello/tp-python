fd3ac63df7bbb3697d644a936aa47402fb98d24d
Valentin Troitiño <valiktroi16@gmail.com>
2019-05-23 22:02:11 +0000

    No sirve

--- TP4/FIN.py
+++
@@ -1,5 +0,0 @@
-def isnumeric(n):
-    if str(type(n)) == "<class 'int'>" or str(type(n)) == "<class 'float'>" or str(type(n)) == "<class 'complex'>":
-        return True
-    else:
-        return False
